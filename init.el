d306af2407e4d9c87e52112392a2b107aa964f9a
Ranul Pallemulle <ranulp96@gmail.com>
2019-10-27 05:13:51 +0530

    Simplified from linux-config.

--- init.el
+++ init.el
@@ -19,131 +19,13 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; try - try packages temporarily
-(use-package try
-  :ensure t)
-
-;; which-key - buffer with keybinding completion options
-(use-package which-key
-  :ensure t
-  :config (which-key-mode))
-
 ;; org-mode bullet style
 (use-package org-bullets
   :ensure t
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
   (add-hook 'org-mode-hook (lambda () (setq fill-column 65)))
-  (add-hook 'org-mode-hook 'turn-on-auto-fill)
-  (add-hook 'org-mode-hook (lambda () (setq org-disputed-keys '(
-			 ([(shift up)] . [(meta up)])
-                         ([(shift down)] . [(meta down)])
-                         ([(shift left)] . [(meta left)])
-                         ([(shift right)] . [(meta right)])
-                         ([(meta return)] . [(control meta return)])
-                         ([(control shift right)] . [(meta shift +)])
-                         ([(control shift left)] . [(meta shift -)])))))
-  (add-hook 'org-mode-hook (lambda () (setq org-replace-disputed-keys t))))
-
-;; Irony mode
-(use-package irony
-  :ensure t
-  :config
-  (add-hook 'c-mode-hook 'irony-mode)
-  (add-hook 'c++-mode-hook 'irony-mode)
-  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
-
-;; Company
-(use-package company
-  :ensure t
-  :config
-  (add-hook 'after-init-hook 'global-company-mode)
-  (add-hook 'org-mode-hook
-	    (lambda () (setq-local company-dabbrev-downcase nil))))
-
-;; Company-irony
-(use-package company-irony
-  :ensure t
-  :config
-  (add-to-list 'company-backends 'company-irony))
-
-;; Company-rtags - push to company-backends when using rtags
-(use-package company-rtags
-  :ensure t)
-
-;; Company-c-headers (for c and c++ headers completion)
-(use-package company-c-headers
-  :ensure t
-  :config
-  (add-to-list 'company-backends 'company-c-headers))
-
-;; flycheck
-(use-package flycheck
-  :ensure t
-  :config
-  (global-flycheck-mode t)
-  (add-hook 'c++-mode-hook (lambda () (setq flycheck-checker 'c/c++-gcc)))
-  (add-hook 'c++-mode-hook (lambda ()
-			     (setq flycheck-gcc-language-standard "c++14")))
-  (add-hook 'c++-mode-hook (lambda ()
-			     (setq flycheck-c/c++-gcc-executable "/usr/bin/g++")))
-  (add-hook 'c-mode-hook (lambda () (setq flycheck-checker 'c/c++-gcc)))
-  (add-hook 'c-mode-hook (lambda ()
-			   (setq flycheck-gcc-language-standard "gnu89")))
-  (add-hook 'c-mode-hook (lambda ()
-			   (setq flycheck-c/c++-gcc-executable "/usr/bin/gcc"))))
-
-;; flycheck-pkg-config - configure flycheck header directories using pkg-config
-(use-package flycheck-pkg-config
-  :ensure t)
-
-;; flycheck-inline - show error messages inline
-(use-package flycheck-inline
-  :ensure t
-  :config
-  (add-hook 'flycheck-mode-hook #'flycheck-inline-mode))
-
-;; Flycheck-irony
-(use-package flycheck-irony
-  :ensure t
-  :config
-  (add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
-
-;; RTags (for cmake ide), needs external daemon
-(use-package rtags
-  :ensure t
-  :config
-  (progn
-    (setq rtags-path "/usr/local/bin/")
-    (setq rtags-autostart-diagnostics t)
-    (setq rtags-completions-enabled t)
-    (rtags-enable-standard-keybindings)
-    (push 'company-rtags company-backends)
-    (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
-    (add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
-    (add-hook 'objc-mode-hook 'rtags-start-process-unless-running)
-    (define-key c-mode-base-map (kbd "M-.")
-      (function rtags-find-symbol-at-point))
-    (define-key c-mode-base-map (kbd "M-,")
-      (function rtags-find-references-at-point))
-    (define-key c-mode-base-map (kbd "M-(")
-      (function rtags-location-stack-back))
-    (define-key c-mode-base-map (kbd "M-)")
-      (function rtags-find-symbol-at-point))
-    (define-key c-mode-base-map (kbd "<C-tab>")
-      (function company-complete))))
-
-;; Flycheck-rtags
-(use-package flycheck-rtags
- :ensure t)
-
-;; cmake-ide
-(use-package cmake-ide
- :ensure t
- :config
- (progn
-   (require 'rtags)
-   (cmake-ide-setup)))
+  (add-hook 'org-mode-hook 'turn-on-auto-fill))
 
 ;; modern-cpp-font-lock for modern c++ highlighting
 (use-package modern-cpp-font-lock
@@ -156,6 +38,7 @@
 (add-hook 'c-mode-common-hook 'auto-fill-mode)
 
 ;; default C compile command for quick compiling
+(require 'format-spec)
 (defun c-compile ()
   "Command for quickly compiling a single C source file."
   (interactive)
@@ -171,6 +54,7 @@
   (define-key c-mode-map (kbd "M-c") 'c-compile))
 
 ;; default C++ compile command for quick compiling
+(require 'format-spec)
 (defun c++-compile ()
   "Command for quickly compiling a single C++ source file."
   (interactive)
@@ -185,21 +69,6 @@
 (with-eval-after-load 'cc-mode
   (define-key c++-mode-map (kbd "M-c") 'c++-compile))
 
-;; Python configuration (need pip install jedi rope flake8 autopep8 yapf black)
-(use-package elpy
-  :ensure t
-  :init
-  (elpy-enable))
-
-(add-hook 'python-mode-hook 'auto-fill-mode)
-
-;; yasnippet
-(use-package yasnippet
-  :ensure t
-  :config
-  (yas-global-mode 1))
-(use-package yasnippet-snippets)
-
 ;; Rust
 (use-package rust-mode
   :ensure t
@@ -210,20 +79,10 @@
 (use-package flymd
   :ensure t)
 
-;; all-the-icons
-(use-package all-the-icons
-  :ensure t)
-
-;; Theme
-;; (use-package gruber-darker-theme ;badger-theme cyberpunk-theme
-;;   :ensure t
-;;   :config
-;;   (load-theme 'gruber-darker t))
-
 (use-package doom-themes
   :ensure t
   :config
-  (load-theme 'doom-sourcerer t))		;doom-nord doom-palenight
+  (load-theme 'doom-sourcerer t))
 
 ; spaceline modeline
 (use-package spaceline
@@ -255,22 +114,6 @@
 (use-package magit
   :ensure t
   :bind (("C-x g" . magit-status)))
-
-;; diff-hl - for highlighting uncommited changes
-(use-package diff-hl
-  :ensure t
-  :config
-  (global-diff-hl-mode t))
-
-;; exec-path-from-shell - import environment variables from shell
-(use-package exec-path-from-shell
-  :ensure t
-  :config
-  (exec-path-from-shell-copy-env "C_INCLUDE_PATH")
-  (exec-path-from-shell-copy-env "CPLUS_INCLUDE_PATH")
-  (exec-path-from-shell-copy-env "LD_LIBRARY_PATH")
-  (exec-path-from-shell-copy-env "PKG_CONFIG_PATH")
-  (exec-path-from-shell-initialize))
 
 ;; rainbow-delimiters
 (use-package rainbow-delimiters
@@ -315,12 +158,6 @@
 (use-package cmake-mode
   :ensure t)
 
-;; multi-term
-(use-package multi-term
-  :ensure t
-  :config
-  (setq multi-term-program "/usr/bin/bash"))
-
 ;; auctex - edit LaTeX
 (use-package auctex
   :defer t
@@ -344,15 +181,6 @@
   (global-set-key (kbd "C->") 'mc/mark-next-like-this)
   (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
   (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this))
-
-;; highlight-symbol - highlight occurrences of the current word
-(use-package highlight-symbol
-  :ensure t
-  :config
-  (global-set-key (kbd "M-h") 'highlight-symbol)
-  (add-hook 'c-mode-common-hook (lambda() (highlight-symbol-nav-mode t)))
-  (add-hook 'python-mode-hook (lambda() (highlight-symbol-nav-mode t)))
-  (add-hook 'emacs-lisp-mode-hook (lambda() (highlight-symbol-nav-mode t))))
 
 ;; web-mode for editing html/css and inline/internal javascript/php, etc
 (use-package web-mode
@@ -380,16 +208,11 @@
 (column-number-mode t)
 (menu-bar-mode -1)
 (global-display-line-numbers-mode)
-(display-time-mode t)
-(setq display-time-default-load-average nil)
 (show-paren-mode 1)
 (setq show-paren-delay 0)
 (add-hook 'org-mode-hook (lambda() (display-line-numbers-mode -1)))
 (add-hook 'doc-view-mode-hook (lambda() (display-line-numbers-mode -1)))
-(add-hook 'eww-mode-hook
-	  (lambda()(define-key eww-mode-map (kbd "M-c") 'eww-toggle-colors)))
 (global-set-key (kbd "C-t") (lookup-key global-map (kbd "C-x 5")))
-(global-set-key (kbd "s-u") 'revert-buffer)
 (set-face-attribute 'default nil :font "hack-12")
 (set-face-attribute 'region nil :background "#d6972b" :foreground "#ffffff")
 (set-face-background 'show-paren-match (face-background 'default))
@@ -511,18 +334,3 @@
 
 (provide 'init)
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (highlight-symbol elpy yasnippet-snippets which-key web-mode use-package try treemacs-projectile treemacs-magit treemacs-icons-dired stickyfunc-enhance spacemacs-theme spaceline-all-the-icons solarized-theme rust-mode rainbow-delimiters org-bullets nyx-theme naysayer-theme multiple-cursors multi-term monokai-theme modern-cpp-font-lock laguna-theme gruvbox-theme flymd flymake flycheck-rtags flycheck-pkg-config flycheck-irony flycheck-inline fancy-battery exotica-theme exec-path-from-shell emmet-mode dracula-theme doom-themes diminish diff-hl cyberpunk-theme cyberpunk-2019-theme cquery counsel-etags company-rtags company-jedi company-irony company-c-headers cmake-mode cmake-ide clang-format ccls badger-theme auctex ample-theme)))
- '(safe-local-variable-values (quote ((setq flycheck-checker (quote c/c++-gcc))))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
